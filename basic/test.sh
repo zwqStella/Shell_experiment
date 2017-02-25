2b00f18b18709f6526980df4c194932e47ae67a2
钟文清 <zwqStella@hotmail.com>
2017-02-25 15:12:51 +0800

    Create test.sh

---
+++ basic/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# use this to test some basic elements in shell
+# variable, parameter and flow control
+str1="Hello,"$1"!"
+f="f"
+if [ $2 == $f ]
+then
+str2="You are a beautiful girl."
+else
+str2="You are a handsome boy."
+fi
+echo $str1
+echo $str2

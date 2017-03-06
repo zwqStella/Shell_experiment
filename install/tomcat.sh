275f366ae30194fde58610789c2f7e965c25ff89
钟文清 <zwqStella@hotmail.com>
2017-03-06 14:50:36 +0800

    Create tomcat.sh

---
+++ install/tomcat.sh
@@ -0,0 +1,9 @@
+#this file hasn't been tested.. waiting for modification
+#/bin/bash
+wget -q -x -p /tomcat -o tomcat-8.tar.gz http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.11/bin/apache-tomcat-8.5.11.tar.gz
+cd /tomcat
+tar -x -f tomcat-8.tar.gz
+rm tomcat-8.tar.gz
+cd tomcat-8
+./bin/startup.sh
+exit 0

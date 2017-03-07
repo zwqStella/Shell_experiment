0799ec677c60d604de54361cd88b13856b6bf992
钟文清 <zwqStella@hotmail.com>
2017-03-07 13:44:49 +0800

    Update jdk.sh

--- install/jdk.sh
+++ install/jdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget -q -x -p ～/java -o jdk1.8.tar.gz http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-i586.tar.gz
-cd /java
+cd ～/java
 tar -x -f jdk1.8.tar.gz
 rm jdk1.8.tar.gz
 echo JAVA_HOME=/home/zwqstella/java/jdk1.8

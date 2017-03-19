901822e3d6baa82b2f72cc6510440f4c8957efd1
钟文清 <zwqStella@hotmail.com>
2017-03-19 18:12:20 +0800

    Update jdk.sh

--- install/jdk.sh
+++ install/jdk.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-wget -q -x -p /java -o jdk1.8.tar.gz http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-i586.tar.gz
-cd /java
-tar -x -f jdk1.8.tar.gz
-rm jdk1.8.tar.gz
-echo JAVA_HOME=/java/jdk1.8
+if [ ! -d "/java2" ]; then
+mkdir /java2
+else
+rm -r /java2
+mkdir /java2 
+fi
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" -q -P /java2 http://download.oracle.com/otn-pub/java/jdk/8u121-b13/e9e7ea248e2c4826b92b3f075a80e441/jdk-8u121-linux-i586.tar.gz
+tar -xzvf /java2/jdk-8u121-linux-i586.tar.gz 
+#rm jdk-8u121-linux-i586.tar.gz
+echo "JAVA_HOME=/java/jdk-8u121-linux-i586
 CLASSPATH=$JAVA_HOME/lib/
 PATH=$PATH:$JAVA_HOME/bin
-export PATH JAVA_HOME CLASSPATH
->> /etc/profile
+export PATH JAVA_HOME CLASSPATH" >> ~/.profile
+source ~/.profile
 java -version
 exit 0

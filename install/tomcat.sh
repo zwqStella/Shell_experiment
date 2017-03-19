a8880b05b08c95146061492edc0b734949bdfa89
钟文清 <zwqStella@hotmail.com>
2017-03-19 18:11:14 +0800

    Update tomcat.sh

--- install/tomcat.sh
+++ install/tomcat.sh
@@ -1,9 +1,8 @@
-#this file hasn't been tested.. waiting for modification
 #/bin/bash
-wget -q -x -p /tomcat -o tomcat-8.tar.gz http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.11/bin/apache-tomcat-8.5.11.tar.gz
+wget -q -P /tomcat http://mirror.bit.edu.cn/apache/tomcat/tomcat-8/v8.5.11/bin/apache-tomcat-8.5.11.tar.gz
 cd /tomcat
-tar -x -f tomcat-8.tar.gz
-rm tomcat-8.tar.gz
-cd tomcat-8
+tar -xzvf apache-tomcat-8.5.11.tar.gz
+rm apache-tomcat-8.5.11.tar.gz
+cd apache-tomcat-8.5.1
 ./bin/startup.sh
 exit 0
